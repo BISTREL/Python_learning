94080a0e4cefda4c7e6f0bd6ea43d9347e38c509
BISTREL <117296325+BISTREL@users.noreply.github.com>
2023-04-18 13:19:33 +0100

    List, Set, Tuples
    Learning

--- List.py
+++ List.py
@@ -1,8 +1,9 @@
 #coding: utf-8
-
+###LIST
 COURSE = ["Phy", "Math", "Chem", "CS", "Geo", "FMath"]
 COURSE1 = ["Art","Cast"]
 
+COURSE[0] = "Geol"
 COURSE.append('Miss')
 COURSE.insert(2, 'Dr')
 COURSE1.extend(COURSE)
@@ -13,3 +14,15 @@ print(COURSE.index('Math'))
 for index, sub in enumerate (COURSE1, start=1):
     print(index, sub)
 
+###Tuples 
+"""Doesn't support assignement (unmutable)"""
+
+COUR = ("Phy", "Math", "Chem", "CS", "Geo", "FMath")
+
+###SET
+
+Ct = {"Phy", "Math", "Chem", "CS", "Geo", "FMath", "Math"}
+art = {"Design", "Math", "His", "Geo", "Math"}
+
+print(Ct.intersection(art))
+print(Ct.union(art))
